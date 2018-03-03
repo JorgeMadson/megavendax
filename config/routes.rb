41a5a5c28cd378547ea25dc5cb9ed9bebc774599
Jorge Madson <filholouco@gmail.com>
2018-03-03 16:43:45 -0300

    resources para todos os objetos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'ingressos#index'
+#Exemplo de rota normal
+#get 'sobre' => ingressos#sobre
+
+#Exemplo de rota de resources 
+resources :ingressos
+resources :pedidos
+#Cada comprador só pode comprar UM ingresso
+resources :compradores
 end
