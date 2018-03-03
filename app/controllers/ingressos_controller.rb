60fcfbba4cf483e2609e2d180096bf96c276bee2
Jorge Madson <filholouco@gmail.com>
2018-03-03 17:19:21 -0300

    Criando e salvando ingressos no banco

--- app/controllers/ingressos_controller.rb
+++ app/controllers/ingressos_controller.rb
@@ -2,10 +2,22 @@ class IngressosController < ApplicationController
     def index
 
     end
+    def show
+        @ingresso = Ingresso.find(params[:id])
+    end
     def new
 
     end
     def create
-        render plain: params[:ingresso].inspect
+        #render plain: params[:ingresso].inspect
+        @ingresso = Ingresso.new(ingresso_params)
+        
+        @ingresso.save
+        redirect_to @ingresso
+    end
+    
+    #Para evitar o erro de atributo proibido, dizemos aqui quais parametros são permitidos
+    private def ingresso_params
+        params.require(:ingresso).permit(:nome, :preco, :taxa, :data)
     end
 end
