78fd39b0401252a2da0276440b548a32cca808c4
Jorge Madson <filholouco@gmail.com>
2018-03-03 18:03:08 -0300

    Pondo um pouco de bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'ingressos#index'
+root 'ingressos#index', as: 'home'
 #Exemplo de rota normal
 #get 'sobre' => ingressos#sobre
 
